dc231952655e3f3fa9c9f59142db556cb618c73a
bruno <brunobermann@gmail.com>
2017-12-06 01:16:30 -0200

    dist/file/* is copied once on create bin directory.

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-mkdir bin > /dev/null 2>&1
+mkdir bin > /dev/null 2>&1 && cp -R dist/file/* bin/
 
 cp -R dist/bin/* bin/
-cp -R dist/file/* bin/
 
 mkdir bin/scripts > /dev/null 2>&1
 cp -R dist/lua bin/scripts/lua
