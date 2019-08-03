ed1b90bb0bf1b02cf1827c045ff1266c5370ae88
Renan Picanço <renan@picsys.com.br>
2019-08-03 13:20:03 -0300

    Automatizando busca por arquivos no cpplint

--- dev/cpplint.sh
+++ dev/cpplint.sh
@@ -4,6 +4,5 @@ wroot_dir="$(dirname $0)"
 cd $wroot_dir/../
 wroot_dir=$PWD
 
-$wroot_dir/dev/build.sh $1 &&
-cd $wroot_dir/bin &&
-gdbserver 10.0.75.1:666 ./wroot;
+mkdir $wroot_dir/dev/cpplint > /dev/null 2>&1
+cpplint $(find $wroot_dir/src -not \( -path $wroot_dir/src/3rdParty -prune \) -name *.cpp -or -name \*.hpp) > $wroot_dir/dev/cpplint/output.txt 2>&1
