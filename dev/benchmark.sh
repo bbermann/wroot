f4892652ff8f90571cf04f95fbd0de521ea2883a
Bruno Bermann <brunobermann@gmail.com>
2019-10-05 00:37:39 -0300

    Atualizando valores de configuracao (server:threads=16) e o tempo de sleep(5) no script de benchmark.

--- dev/benchmark.sh
+++ dev/benchmark.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ./run.sh prod &
-sleep 1 # seconds
-ab -n 100000 -c 50 http://127.0.0.1:8000/404 > logs/ab_output.log 2>&1 &
+sleep 5 # seconds
+ab -n 100000 -c 50 http://127.0.0.1:8000/ > logs/ab_output.log 2>&1 &
 sleep 1 # seconds
 htop
 killall ab
