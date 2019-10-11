c6f32d7fe3951d68734d4ca4a5caf4ac7b76b93a
Bruno Bermann <brunobermann@gmail.com>
2019-10-10 23:24:23 -0300

    Ajustando .gitattributes

--- dev/benchmark.sh
+++ dev/benchmark.sh
@@ -3,7 +3,7 @@
 ./run.sh prod &
 sleep 3 # seconds
 # Fast test: Execute (-c) concurrent http request until (implicit by -t) 50k requests or (-t) seconds timeout.
-ab -t 20 -c 50 http://127.0.0.1:8000/ > logs/ab_output.log 2>&1 &
+ab -t 10 -c 50 http://127.0.0.1:8000/ > logs/ab_output.log 2>&1 &
 # Long duration test: Execute (-c) concurrent http request until reach the total of (-n) requests.
 # ab -n 1000000 -c 500 http://127.0.0.1:8000/ > logs/ab_output.log 2>&1 &
 htop
