45ee5c826f4aa17c48767b9e18b996ed36ef064b
Bruno Bermann <brunobermann@gmail.com>
2019-10-26 19:27:20 -0300

    Atualizando dev/docker.ps1 para funcionar com docker-compose e ajustando path do volume e versão no docker-compose.yml

--- dev/docker.ps1
+++ dev/docker.ps1
@@ -6,19 +6,16 @@ if ($args[0] -eq "build") {
     }
 
     echo "Rebuilding docker image 'wroot'..."
-    docker build -t wroot ../
-} elseif ($args[0] -eq "run") {
-    echo "Running wroot server..."
-    docker run -d -p 8000:8000/tcp -p 666:666/tcp -p 22:22/tcp -v G:\Programacao\wroot:/wroot wroot bash -c "cd /wroot/dev && ./run.sh"
+    docker-compose build wroot-dev
 } elseif ($args[0] -eq "ssh") {
     echo "Running ssh into wroot container..."
     ssh root@localhost
 } elseif ($args[0] -eq "stop") {
     echo "Stopping wroot container..."
-    docker stop $(docker ps -q --filter ancestor=wroot)
+    docker-compose down wroot-dev
 } else {
     echo "Running wroot container"
-    docker run -d -p 8000:8000/tcp -p 666:666/tcp -p 22:22/tcp -v G:\Programacao\wroot:/wroot wroot
+    docker-compose up -d wroot-dev
     sleep 5 # seconds
     ssh root@localhost
 }
