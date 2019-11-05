96121bac2ee0e28fbd1f9a8bf75705b9ee7fe5e2
Bruno Bermann <brunobermann@gmail.com>
2019-11-05 19:01:52 -0300

    Adicionando arquivos do HttpClient, sem implementação por enquanto.

---
+++ src/include/network/http/HttpClient.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bbermann on 04/11/2019.
+//
+
+#include "HttpClient.hpp"
---
+++ src/include/network/http/HttpClient.hpp
@@ -0,0 +1,14 @@
+//
+// Created by bbermann on 04/11/2019.
+//
+
+
+#pragma once
+
+
+class HttpClient {
+
+};
+
+
+
